fbbf985a13cb6cc4fcf0d196123ae0c767b38393
marvinlartey <75137498+marvinlartey@users.noreply.github.com>
2023-08-23 15:41:55 +0000

    added list of patients

--- lib/Pages/staff/list_of_patients.dart
+++ lib/Pages/staff/list_of_patients.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:roseth_eye_care/Pages/staff/parient_details.dart';
 
 class PatientList extends StatefulWidget {
   const PatientList({super.key});
@@ -15,7 +16,40 @@ class _PatientListState extends State<PatientList> {
       appBar: AppBar(
         title: const Text('Patient List'),
       ),
-      body: _buildPatientList(),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future:
+            fetchPatientsFromFirestore(), // Define this function to fetch data
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error fetching data'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No patients available.'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> patientData = snapshot.data![index];
+                return ListTile(
+                  title: Text(patientData['name']),
+                  subtitle: Text(
+                      'ID: ${patientData['idNumber']}, Age: ${patientData['age']}'),
+                  onTap: () {
+                    // Navigate to PatientDetailsPage with selected patient data
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PatientDetailsPage(patientData),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 
@@ -51,5 +85,23 @@ class _PatientListState extends State<PatientList> {
         // Add more patient details as needed
       ),
     );
+  }
+
+  Future<List<Map<String, dynamic>>> fetchPatientsFromFirestore() async {
+    List<Map<String, dynamic>> patients = [];
+
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await FirebaseFirestore.instance.collection('patients').get();
+
+      for (var doc in querySnapshot.docs) {
+        patients.add(doc.data());
+      }
+
+      return patients;
+    } catch (e) {
+      print('Error fetching patients: $e');
+      return [];
+    }
   }
 }
